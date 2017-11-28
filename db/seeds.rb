6979766098f7111a0339156601fb923dea703dca
Roman Kislov <gzeek57@gmail.com>
2017-11-28 15:11:30 +0100

    task manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Started seed"
+puts "Emptying DB!"
+
+Task.destroy_all
+
+# status = %w(true false)
+status = [true, false]
+10.times do
+  tasks = Task.new(name: Faker::Simpsons.character, description: Faker::Simpsons.quote, status: status.sample)
+
+  puts "#{tasks.name} #{tasks.status} saved!" if tasks.save
+end
+
+puts "Seeding finished!"
